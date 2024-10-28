ac6e43a276be2a248c7825655db317aab09be65d
DoubledConG <992196871@qq.com>
2024-10-28 22:44:56 +0800

    Update the styles

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
 "use client";
 import { useState, useEffect } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
 export default function Home() {
@@ -31,40 +29,34 @@ export default function Home() {
   );
 
   return (
-    <div className="container mx-auto flex-1 p-8">
+    <div className="container mx-auto flex-1 p-6 bg-blue-gray-100">
+      <h1 className="text-3xl text-center font-semibold mb-6"> Awesome Website </h1>
       <div className="mb-6">
-        <Input
-          type="search"
-          placeholder="Search websites..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full bg-white"
+        <input
+          className="w-full border p-3 rounded-lg focus:outline-none focus:shadow bg-white"
+          placeholder="Search website..."
+          onChange={e => setSearchTerm(e.target.value)}
         />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-6">
         {filteredWebsites.map((website, index) => (
           <Link
-            className="flex flex-col flex-grow"
+            className="flex flex-1 transition-all hover:-translate-y-1"
             key={index}
             href={website.url}
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Card className="overflow-hidden flex flex-col flex-1">
-              <CardHeader className="pb-0">
-                <CardTitle className="text-lg line-clamp-1">
+            <div className="flex flex-1 transition hover:shadow border rounded-lg bg-white">
+              <div className="p-4">
+                <h3 className="mb-2 text-xl font-bold line-clamp-1">
                   {website.title}
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <div
-                  className="text-sm text-muted-foreground line-clamp-2"
-                  title={website.description}
-                >
-                  {website.description || "No description"}
-                </div>
-              </CardContent>
-            </Card>
+                </h3>
+                <p className="text-sm line-clamp-2 text-blue-gray-300">
+                  {website.description || 'No description'}
+                </p>
+              </div>
+            </div>
           </Link>
         ))}
       </div>
