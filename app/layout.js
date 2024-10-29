f394374754df0bae95120dc158c347a425a51fd9
DoubledConG <992196871@qq.com>
2024-10-29 20:02:09 +0800

    Delete useless files

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,17 @@
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Awesome Website - Collect excellent websites",
   description: "Some of my favorite sites",
   icons: {
     icon: "/favicon.svg",
-  },
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
       </body>
     </html>
