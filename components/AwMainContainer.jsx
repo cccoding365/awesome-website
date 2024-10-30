152d256a64313437e49eac52d0ea93d54f6c7b36
DoubledConG <992196871@qq.com>
2024-10-30 23:23:29 +0800

    Use the apple icons and color icons

--- components/AwMainContainer.jsx
+++ components/AwMainContainer.jsx
@@ -2,6 +2,70 @@
 import { useState, useEffect } from "react"
 import Link from "next/link"
 
+function randomBgColor(str) {
+    const tailwindColors = [
+        "bg-slate-600",
+        "bg-gray-600",
+        "bg-zinc-600",
+        "bg-neutral-600",
+        "bg-stone-600",
+        "bg-red-600",
+        "bg-orange-600",
+        "bg-amber-600",
+        "bg-yellow-600",
+        "bg-lime-600",
+        "bg-green-600",
+        "bg-emerald-600",
+        "bg-teal-600",
+        "bg-cyan-600",
+        "bg-sky-600",
+        "bg-blue-600",
+        "bg-indigo-600",
+        "bg-violet-600",
+        "bg-purple-600",
+        "bg-fuchsia-600",
+        "bg-pink-600",
+        "bg-rose-600"
+    ]
+    let hash = 0
+    for (let i = 0; i < str.length; i++) {
+        hash = (hash + str.charCodeAt(i) * i) % 65536
+    }
+    const index = Math.abs(hash) % tailwindColors.length
+    return tailwindColors[index]
+}
+
+function WebsiteIcon({ baseUrl, title }) {
+    const [isImageAvailable, setIsImageAvailable] = useState(null)
+
+    useEffect(() => {
+        const img = new Image()
+        img.onload = () => setIsImageAvailable(true)
+        img.onerror = () => setIsImageAvailable(false)
+        img.src = baseUrl + "apple-touch-icon.png"
+    }, [baseUrl])
+
+    return (
+        <div className="rounded-lg size-16 flex-shrink-0 mr-3 overflow-hidden">
+            {isImageAvailable ? (
+                <div className="size-16">
+                    <img src={baseUrl + "apple-touch-icon.png"} />
+                </div>
+            ) : (
+                <div
+                    className={`${randomBgColor(
+                        title
+                    )} size-16 flex justify-center items-center`}
+                >
+                    <span className="text-3xl font-extrabold font-serif text-white">
+                        {title.charAt(0).toUpperCase()}
+                    </span>
+                </div>
+            )}
+        </div>
+    )
+}
+
 export default function AwMainContainer() {
     const [websites, setWebsites] = useState([])
     const [categories, setCategories] = useState([])
@@ -32,27 +96,28 @@ export default function AwMainContainer() {
 
     return (
         <div>
-            <div className="px-10 mt-6">
-                <h3 className="text-2xl font-semibold">All</h3>
-            </div>
             <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 mb-6 p-6">
                 {filteredWebsites.map((website, index) => (
                     <Link
-                        className="flex flex-1 transition-all hover:-translate-y-1"
+                        className="transition-all hover:-translate-y-1"
                         key={index}
                         href={website.url}
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <div className="flex flex-1 items-center transition hover:shadow border rounded-lg bg-white p-4">
-                            <div className="text-3xl font-extrabold p-3 border-2 text-center mr-4 rounded-lg size-16 flex-shrink-0">
-                                {website.title.charAt(0).toUpperCase()}
-                            </div>
+                        <div className="flex overflow-hidden items-center transition hover:shadow border rounded-lg bg-white p-4">
+                            <WebsiteIcon
+                                baseUrl={website.url}
+                                title={website.title}
+                            ></WebsiteIcon>
                             <div>
                                 <h3 className="mb-2 text-xl font-bold line-clamp-1">
                                     {website.title}
                                 </h3>
-                                <p className="text-sm line-clamp-1 text-blue-gray-300" title={website.description}>
+                                <p
+                                    className="text-sm line-clamp-1 text-blue-gray-300"
+                                    title={website.description}
+                                >
                                     {website.description || "No description"}
                                 </p>
                             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
-
 /** @type {import('tailwindcss').Config} */
-module.exports = withMT({
+module.exports = {
 	content: [
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}"
 	],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
-});
+};
