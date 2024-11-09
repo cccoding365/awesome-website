8744ec287854477766c504e01317e9cbeb965b08
DoubledConG <992196871@qq.com>
2024-11-09 20:17:10 +0800

    Update the icons style

--- app/components/AwMainContainer.jsx
+++ app/components/AwMainContainer.jsx
@@ -3,66 +3,14 @@ import { useState, useEffect } from "react"
 import Link from "next/link"
 import { useStore } from "@/app/store"
 
-function randomBgColor(str) {
-    const bgColors = [
-        "bg-slate-600",
-        "bg-gray-600",
-        "bg-zinc-600",
-        "bg-neutral-600",
-        "bg-stone-600",
-        "bg-red-600",
-        "bg-orange-600",
-        "bg-amber-600",
-        "bg-yellow-600",
-        "bg-lime-600",
-        "bg-green-600",
-        "bg-emerald-600",
-        "bg-teal-600",
-        "bg-cyan-600",
-        "bg-sky-600",
-        "bg-blue-600",
-        "bg-indigo-600",
-        "bg-violet-600",
-        "bg-purple-600",
-        "bg-fuchsia-600",
-        "bg-pink-600",
-        "bg-rose-600"
-    ]
-    let hash = 0
-    for (let i = 0; i < str.length; i++) {
-        hash = (hash + str.charCodeAt(i) * i) % 65536
-    }
-    const index = Math.abs(hash) % bgColors.length
-    return bgColors[index]
-}
-
 function WebsiteIcon({ baseUrl, title }) {
-    const [isImageAvailable, setIsImageAvailable] = useState(null)
-
-    useEffect(() => {
-        const img = new Image()
-        img.onload = () => setIsImageAvailable(true)
-        img.onerror = () => setIsImageAvailable(false)
-        img.src = baseUrl + "apple-touch-icon.png"
-    }, [baseUrl])
-
     return (
         <div className="rounded-lg size-16 flex-shrink-0 mr-3 overflow-hidden">
-            {isImageAvailable ? (
-                <div className="size-16">
-                    <img src={baseUrl + "apple-touch-icon.png"} />
-                </div>
-            ) : (
-                <div
-                    className={`${randomBgColor(
-                        title
-                    )} size-16 flex justify-center items-center bg-accent-100`}
-                >
-                    <span className="text-3xl font-extrabold font-serif text-text-200">
-                        {title.charAt(0).toUpperCase()}
-                    </span>
-                </div>
-            )}
+            <div className="size-16 flex justify-center items-center bg-accent-100">
+                <span className="text-3xl font-extrabold font-serif text-text-200">
+                    {title.charAt(0).toUpperCase()}
+                </span>
+            </div>
         </div>
     )
 }
